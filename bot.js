2958ed8c74092c49d1822b209273e42fc92de2a4
Matt White <mattwhite06@googlemail.com>
2020-03-21 16:49:07 +0000

    Added !coffeetime command

--- bot.js
+++ bot.js
@@ -3,54 +3,80 @@ require('dotenv').config();
 const Discord = require('discord.js');
 var schedule = require('node-schedule');
 var client = new Discord.Client();
+
 var channel = null;
+var job = null;
 
 client.on('ready', () => 
 {
-    console.log(`Logged in as ${client.user.tag}!`);
+    console.log('Logged in as ${client.user.tag}!');
 
     channel = client.channels.cache.find( channel => channel.name == 'general');
     if (channel != null)
     {
       var rule = new schedule.RecurrenceRule();
-      rule.dayOfWeek = schedule.Range(1,5); // Mon-Fri
+      rule.dayOfWeek = [1, 2, 3, 4, 5]; // Mon-Fri
       rule.hour = 15;
       rule.minute = 0;
 
-      var j = schedule.scheduleJob(rule, function() {
+      job = schedule.scheduleJob('CoffeeTimeReminder', rule, function() {
           channel.send('Coffee time? :coffee:');
         });
 
+      if (job == null)
+      {
+        console.log('Failed to start CoffeeTimeReminder job.');
+        client.destroy();
+        return;
+      }
+    
       client.user.setActivity(', Waiting...', { type: 'WATCHING' });
     }
   });
 
 client.login(process.env.DISCORD_TOKEN);
 
-client.on('message', msg => {
-    if (!msg.author.bot)
-    {
-      if (msg.content.toLowerCase().includes('coffee')) {
-        if (msg.author.tag === 'CitrusySteve#5217') {   // security check  :D
-          if (channel != null) {
-            msg.reply('Heretic!');
-          }
-        }
-        else {
-          msg.react('☕');
-        }
-      }
+function onCommand(msg) {
+  var command  = msg.content.toLowerCase();
+  if (command === '!coffeetime') {
+    if (job != null) {
+      msg.reply('The next scheduled CoffeeTime is ' + job.nextInvocation());
+    }
+    else {
+      msg.reply('There is currently no next scheduled CoffeeTime...   PANIC!');
+    }
+  }
+}
 
-      if (msg.author.tag === 'EviK#5094')
-      { 
-          if (msg.content.toLowerCase().includes('matthew'))
-          {
-            msg.reply('You mean Matt, not Matthew?');
+client.on('message', msg => {
+  if (!msg.author.bot)
+  {
+    if (msg.content.startsWith('!')) {
+      onCommand(msg);
+    }
+    else {
+        if (msg.content.toLowerCase().includes('coffee')) {
+          if (msg.author.tag === 'CitrusySteve#5217') {   // security check  :D
+            if (channel != null) {
+              msg.reply('Heretic!');
+            }
           }
-          if (msg.content.toLowerCase().includes('mathew'))
-          {
-            msg.reply('You mean Matt, not Mathew?');
+          else {
+            msg.react('☕');
           }
+        }
+
+        if (msg.author.tag === 'EviK#5094')
+        { 
+            if (msg.content.toLowerCase().includes('matthew'))
+            {
+              msg.reply('You mean Matt, not Matthew?');
+            }
+            if (msg.content.toLowerCase().includes('mathew'))
+            {
+              msg.reply('You mean Matt, not Mathew?');
+            }
+        }
       }
     }
   });
